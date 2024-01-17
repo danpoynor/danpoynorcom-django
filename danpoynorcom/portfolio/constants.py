20998ffd3bd8cfc082ad04d3cd88b2474031f8b8
Dan Poynor <danpoynor@gmail.com>
2024-01-16 19:19:40 -0600

    Create constants for models and views

--- danpoynorcom/portfolio/constants.py
+++ danpoynorcom/portfolio/constants.py
@@ -1,3 +1,5 @@
+from .models import Client, Industry, Market, MediaType, Role, Project, ProjectItem
+
 PAGINATE_BY = 24
 SELECTED_CLIENT_IDS = [42, 65, 4, 83, 37]
 SELECTED_INDUSTRY_IDS = [14, 32, 8, 3, 21]
@@ -6,3 +8,5 @@ SELECTED_ROLE_IDS = [12, 24, 22, 2, 8]
 HIGHLIGHTED_INDUSTRY_IDS = [3, 13, 32, 8, 21, 24]
 HIGHLIGHTED_MEDIA_TYPE_IDS = [28, 3, 21, 36, 38]
 HIGHLIGHTED_ROLE_IDS = [12, 24, 22, 2, 8]
+VIEW_NAMES = ['home', 'portfolio', 'about', 'contact', 'client_list', 'industry_list', 'market_list', 'mediatype_list', 'role_list']
+MODELS = [Client, Industry, Market, MediaType, Role, Project, ProjectItem]
