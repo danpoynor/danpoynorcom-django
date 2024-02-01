ec5a5b83087c87d4e4d052ebd71955571f3c0f4a
Dan Poynor <danpoynor@gmail.com>
2024-02-01 16:16:58 -0600

    Edit selected industry id: should be Financial Services instead of Food Services

--- danpoynorcom/portfolio/constants.py
+++ danpoynorcom/portfolio/constants.py
@@ -2,7 +2,7 @@ from .models import Client, Industry, Market, MediaType, Role, Project, ProjectI
 
 PAGINATE_BY = 24
 SELECTED_CLIENT_IDS = [42, 65, 4, 83, 37]
-SELECTED_INDUSTRY_IDS = [14, 32, 8, 3, 21]
+SELECTED_INDUSTRY_IDS = [13, 32, 8, 3, 21]
 SELECTED_MEDIA_TYPE_IDS = [28, 3, 21, 36, 38]
 SELECTED_ROLE_IDS = [12, 24, 22, 2, 8]
 HIGHLIGHTED_INDUSTRY_IDS = [3, 13, 32, 8, 21, 24]
